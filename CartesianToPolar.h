8ae8b910f7cb9f22ffa642b90e868c047d5de74e
christos <christos@cern.ch>
2021-06-18 22:33:31 +0200

    Add fast impl ala Max

---
+++ CartesianToPolar.h
@@ -0,0 +1,60 @@
+#include <cmath>
+#include <iostream>
+struct cartesian
+{
+  double x{ 0. };
+  double y{ 0. };
+};
+
+struct polar
+{
+  double r{ 0. };
+  double phi{ 0. };
+};
+
+struct fastCache
+{
+  double prevX{ 0. };
+  double prevY{ 0. };
+  double prevR{ 0. };
+  double prevPhi{ 0. };
+  int steps{ -1 };
+};
+
+inline polar
+precisePol(const cartesian& cart)
+{
+  return polar{ std::sqrt(cart.x * cart.x + cart.y * cart.y),
+                std::atan2(cart.y, cart.x) };
+}
+
+inline polar
+fastPol(const cartesian& cart, fastCache& cache, bool forceReset = false)
+{
+  polar result;
+  if (cache.steps == -1 || cache.steps > 10 || forceReset) {
+    result = precisePol(cart);
+    cache.steps = 0;
+  } else {
+    const double invR = 1. / cache.prevR;
+    const double invR2 = invR * invR;
+    const double dX = cart.x - cache.prevX;
+    const double dY = cart.y - cache.prevY;
+    const double firstRterm = cart.x * invR * dX + cart.y * invR * dY;
+    const double newR = cache.prevR + firstRterm;
+    const double firstPhiterm = -cart.y * invR2 * dX + cart.x * invR2 * dX;
+    double newPhi = cache.prevPhi - firstPhiterm;
+    if (newPhi > M_PI) {
+      newPhi -= 2. * M_PI;
+    }
+    result = { newR, newPhi };
+    std::cout << newR << std::endl;
+  }
+  cache.prevY = cart.y;
+  cache.prevX = cart.x;
+  cache.prevR = result.r;
+  cache.prevPhi = result.phi;
+  ++cache.steps;
+  return result;
+}
+
---
+++ testAccuracy.cxx
@@ -0,0 +1,23 @@
+#include "CartesianToPolar.h"
+#include <cmath>
+#include <iostream>
+using dbl = std::numeric_limits<double>;
+
+int
+main()
+{
+  double r0{ 1250 }, phi{ 1.6 };
+  fastCache cache{};
+  for (unsigned int i = 0; i < 100; ++i) {
+    double r1 = r0 + 5 + i * 1;
+    cartesian init{ r1 * std::cos(phi), r1 * std::sin(phi) };
+    polar exact = precisePol(init);
+    polar fast = fastPol(init, cache);
+    std::cout << " exact r : " << exact.r << " fast r : " << fast.r
+              << " % diff " << std::abs(exact.r - fast.r) / exact.r << '\n';
+    std::cout << " exact phi : " << exact.phi << " fast phi : " << fast.phi
+              << " % diff " << std::abs(exact.phi - fast.phi) / exact.phi
+              << '\n';
+  }
+  return 0;
+}
